db063105525e70dd5f6084bc02291e03fb80d6da
EvanBurnette <34970211+EvanBurnette@users.noreply.github.com>
2023-04-27 07:57:49 -0700

    Add functions to worker

--- src/routes/worker.ts
+++ src/routes/worker.ts
@@ -1,3 +1,33 @@
-export const add = (a: number, b: number) => {
-	return a + b;
+import { isEqual } from 'lodash';
+
+type Counts = { [key: string]: number };
+
+let dictionary: string[] = [];
+let dictionaryMap: { word: string; counts: Counts }[] = [];
+
+export const setDictionary = (dictionary_raw: string) => {
+	dictionary = dictionary_raw.split('\n');
+	createDictionaryMap();
+	console.debug(dictionaryMap.slice(0, 10));
+};
+
+const countLetters = (word: string) => {
+	const counts: Counts = {};
+	for (const letter of word) {
+		counts[letter] ? counts[letter]++ : (counts[letter] = 1);
+	}
+	return counts;
+};
+
+const createDictionaryMap = () => {
+	dictionaryMap = dictionary.map((word) => ({ word: word, counts: countLetters(word) }));
+};
+
+export const getWords = (jumbledWord: string) => {
+	const jWordCounts = countLetters(jumbledWord);
+	return dictionaryMap
+		.filter((wordAndCounts) => {
+			return isEqual(wordAndCounts.counts, jWordCounts);
+		})
+		.map((word) => word.word);
 };
