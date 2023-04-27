1ab4a46f2363af7948e753f78c0dacad1bfb0b55
EvanBurnette <34970211+EvanBurnette@users.noreply.github.com>
2023-04-27 11:43:35 -0700

    Delete store.ts

--- src/routes/store.ts
+++
@@ -1,5 +0,0 @@
-import { readable } from 'svelte/store';
-
-export const xStyle = readable('btn variant-filled-tertiary py-0.5 px-2.5 rounded', (set) => {
-	//do nothing because it's a constant value
-});
