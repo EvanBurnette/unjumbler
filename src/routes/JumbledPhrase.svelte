12c7e03a60368a642aa70400382469f86340b308
EvanBurnette <34970211+EvanBurnette@users.noreply.github.com>
2023-04-29 21:42:39 -0700

    Uppercase jumbled phrase

--- src/routes/JumbledPhrase.svelte
+++ src/routes/JumbledPhrase.svelte
@@ -1,15 +1,17 @@
 <script lang="ts">
 	import { jumbledPhraseStore, jumber } from './stores';
 	let jumbledPhrase = '';
+	let upperPhrase: string;
 	$: jumbledPhrase = Object.entries($jumbledPhraseStore)
 		.filter(([k, v]) => v !== 0)
 		.map(([k, v]) => {
 			return new Array(v).fill(k).join('');
 		})
 		.join('');
+	$: upperPhrase = jumbledPhrase.toUpperCase();
 </script>
 
 <label class="label" for="phrase">
 	<span> {$jumber} Jumbled Letters </span>
-	<input bind:value={jumbledPhrase} id="phrase" type="text" class="input" />
+	<input bind:value={upperPhrase} id="phrase" type="text" class="input" />
 </label>
