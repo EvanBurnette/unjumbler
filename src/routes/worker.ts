e6e74ed294a6683c668458a52e2b263374113d44
EvanBurnette <34970211+EvanBurnette@users.noreply.github.com>
2023-05-04 10:23:52 -0700

    Add mobile switch so we only use performance cores but with a big assumption that every phone has at least 2 performance cores

--- src/routes/worker.ts
+++ src/routes/worker.ts
@@ -54,38 +54,12 @@ export const getWords = (jumbledWord: string) => {
 		.map((word) => word.word);
 };
 
-// const _getPhrases = (
-// 	jumbledPhrase: Counts,
-// 	foundPhrases: string[],
-// 	addSolutionProxy: Function,
-// 	idx = 0,
-// 	phrase = ''
-// ) => {
-// 	//base case: if we've used up all the words then the phrase is good
-// 	if (idx >= subDictionaries.length) {
-// 		// foundPhrases.push(phrase);
-// 		addSolutionProxy(phrase);
-// 		return;
-// 	}
-// 	outerLoop: for (const { word: word, counts: counts } of subDictionaries[idx]) {
-// 		const jumbledPhrase_clone = structuredClone(jumbledPhrase);
-// 		// console.debug(jumbledPhrase_clone);
-// 		// if each jumbledPhrase_clone[key] - counts[key] of current doesn't go negative
-// 		for (const key in counts) {
-// 			jumbledPhrase_clone[key] -= counts[key];
-// 			//early exit because we ran out of a particular letter
-// 			if (jumbledPhrase_clone[key] < 0) continue outerLoop;
-// 		}
-// 		//add word to phrase and recursively call
-// 		_getPhrases(jumbledPhrase_clone, foundPhrases, addSolutionProxy, idx + 1, `${phrase} ${word}`);
-// 	}
-// };
-
 const numCpus = navigator.hardwareConcurrency;
+const isMobile = navigator.maxTouchPoints !== 0;
 
 const subWorkers = [];
-for (let i = 0; i < Math.floor(numCpus / 1); i++) {
-	// for (let i = 0; i < 1; i++) {
+let concurrency = isMobile ? 2 : Math.max(2, Math.floor(numCpus / 2));
+for (let i = 0; i < concurrency; i++) {
 	const subWorker = new ComlinkWorker<typeof import('./subWorker')>(
 		new URL('/subWorker', import.meta.url)
 	);
