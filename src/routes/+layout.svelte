14ea64a8fa1dab98b52bbdd94f2f7f7d0f2addbe
EvanBurnette <34970211+EvanBurnette@users.noreply.github.com>
2023-05-05 10:41:12 -0700

    Add custom modal for About page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -13,26 +13,64 @@
 
 	import { LightSwitch } from '@skeletonlabs/skeleton';
 
-	import { Modal, modalStore } from '@skeletonlabs/skeleton';
-	import type { ModalSettings, ModalComponent } from '@skeletonlabs/skeleton';
-	const aboutModal = () => {
-		const alert: ModalSettings = {
-			type: 'alert',
-			// Data
-			title: 'About Unjumbler',
-			body: 'Ruin the fun of the popular newspaper word game <a href="https://www.google.com/search?q=jumble&tbm=isch" target="_blank">Jumble™️</a>',
-			// image: 'https://upload.wikimedia.org/wikipedia/commons/6/6b/Jumble_puzzle.png',
-			// <br>Example Jumble style puzzle from Lord Belbury, scientist cartoon by BAUSCHRON on wikimedia <a href="https://creativecommons.org/licenses/by-sa/4.0">CC BY-SA 4.0</a>
-			buttonTextCancel: 'Close'
-		};
-		modalStore.trigger(alert);
+	// import { Modal, modalStore } from '@skeletonlabs/skeleton';
+	// import type { ModalSettings, ModalComponent } from '@skeletonlabs/skeleton';
+	// const aboutModal = () => {
+	// 	const alert: ModalSettings = {
+	// 		type: 'alert',
+	// 		// Data
+	// 		title: 'About Unjumbler',
+	// 		body: 'Ruin the fun of the popular newspaper word game <a href="https://www.google.com/search?q=jumble&tbm=isch" target="_blank">Jumble™️</a>',
+	// 		// image: 'https://upload.wikimedia.org/wikipedia/commons/6/6b/Jumble_puzzle.png',
+	// 		// <br>Example Jumble style puzzle from Lord Belbury, scientist cartoon by BAUSCHRON on wikimedia <a href="https://creativecommons.org/licenses/by-sa/4.0">CC BY-SA 4.0</a>
+	// 		buttonTextCancel: 'Close'
+	// 	};
+	// 	modalStore.trigger(alert);
+	// };
+	let aboutDialog: HTMLDialogElement = { open: false };
+	let dialogOpen = false;
+	export const openAbout = () => {
+		dialogOpen = true;
+		aboutDialog.show();
 	};
+
+	const onClickOutSide = (e: MouseEvent) => {
+		console.debug(e);
+		const { left, right, top, bottom } = aboutDialog.getBoundingClientRect();
+		const { clientX, clientY } = e;
+		if (clientX > right || clientX < left || clientY > bottom || clientY < top) {
+			aboutDialog.close();
+		}
+		dialogOpen = false;
+		aboutDialog.close();
+	};
+	let blurBackground = '';
+	$: {
+		blurBackground = dialogOpen ? 'z-20 opacity-70' : 'opacity-0 z-[-10]';
+	}
 </script>
 
+<div on:click={onClickOutSide} class="w-[100vw] h-[100vh] absolute bg-black {blurBackground}" />
+<dialog
+	on:keydown={(e) => {
+		if (e.key === 'Escape') {
+			aboutDialog.close();
+			dialogOpen = false;
+		}
+	}}
+	bind:this={aboutDialog}
+	class="modal w-modal-wide absolute top-[10%] z-30 bg-surface-100-800-token text-primary-400"
+>
+	<p>
+		Ruin the fun of the popular newspaper word game <a
+			href="https://www.google.com/search?q=jumble&tbm=isch"
+			target="_blank">Jumble™️</a
+		>
+	</p>
+</dialog>
 <!-- App Shell -->
 <AppShell>
-	<!-- <div class="grid border-2 items-center"> -->
-	<Modal width="w-modal-slim" />
+	<!-- <Modal width="w-modal-slim" /> -->
 	<!-- </div> -->
 	<svelte:fragment slot="header">
 		<!-- App Bar -->
@@ -42,7 +80,7 @@
 			</svelte:fragment>
 			<svelte:fragment slot="trail">
 				<LightSwitch />
-				<button class="btn btn-sm variant-ghost-surface" on:click={aboutModal}> About </button>
+				<button class="btn btn-sm variant-ghost-surface" on:click={openAbout}> About </button>
 				<a
 					class="btn btn-sm variant-ghost-surface"
 					href="https://github.com/EvanBurnette/unjumbler"
